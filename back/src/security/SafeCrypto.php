cfb9f2463d3efb9ef7c9014a1439622b87e8a1c5
root <hcouto@softexpert.com>
2024-03-19 13:41:12 -0300

    test

--- back/src/security/SafeCrypto.php
+++ back/src/security/SafeCrypto.php
@@ -2,13 +2,13 @@
     declare(strict_types=1);
     function SafeCrypto(string $text, string $type): string {
         if ( $type == "Html") {
-            require "functions/CodifyHtml.php";
+            require_once "functions/CodifyHtml.php";
             return (CodifyHtml($text));
         } else if ( $type == "Encrypt") {
-            require "functions/SafeEncrypt.php";
+            require_once "functions/SafeEncrypt.php";
             return (SafeEncrypt($text));
         } else if ( $type == "Decrypt") {
-            require "functions/SafeDecrypt.php";
+            require_once "functions/SafeDecrypt.php";
             return (SafeDecrypt($text));
         } else {
             return ("FALSE");
